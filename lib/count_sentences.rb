724a164788a902f80d6685241c6f6072d9c87807
Andrea Lovewell <andy.lovewell@gmail.com>
2019-02-05 06:06:40 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -10,13 +10,13 @@ class String
     def question?
       self.end_with?("?")
     end
-    
+
     def exclamation?
       self.end_with?("!")
     end
-    
+
     def count_sentences
       self.split(/[.!?]/).reject {|x| x.empty?}.size
     end
-    
+
 end
