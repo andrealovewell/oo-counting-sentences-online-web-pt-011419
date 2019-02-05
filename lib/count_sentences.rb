4fb6f833bdda4ab5a8f4669713a06f8da098ed90
Andrea Lovewell <andy.lovewell@gmail.com>
2019-02-05 06:06:35 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,21 @@ require 'pry'
 
 class String
 
-  def sentence?
-    self.end_with?(".")
-  end
+  
+    def sentence?
+      self.end_with?(".")
+    end
 
-  def question?
-    self.question?
-  end
-
-  def exclamation?
-    self.exclamation?
-  end
-
-  def count_sentences
+    def question?
+      self.end_with?("?")
+    end
     
-  end
+    def exclamation?
+      self.end_with?("!")
+    end
+    
+    def count_sentences
+      self.split(/[.!?]/).reject {|x| x.empty?}.size
+    end
+    
 end
