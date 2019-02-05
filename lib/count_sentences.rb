bfecb1ef6c610b3b4a2b3ee76a28b12ab985394c
Andrea Lovewell <andy.lovewell@gmail.com>
2019-02-05 06:04:54 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,22 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    if("Hi, my name is Sophie.".end_with?("."))
-      return true
-    else
-      return false
-
+    self.end_with?(".")
   end
-
+  
   def question?
-
+    self.question?
   end
 
   def exclamation?
-
+    self.exclamation?
   end
 
   def count_sentences
-
+    
   end
 end
